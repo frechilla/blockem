3d1b076f02eb9948e52130ec72c709b1c0403e3d
frechilla <frechilla@b4ae199f-ef0c-4245-bc35-8706ab30845c>
2010-07-13 17:57:53 +0000

    changed blocking queue license

--- src/g_blocking_queue.h
+++ src/g_blocking_queue.h
@@ -1,28 +1,40 @@
 // ============================================================================
-// Copyright 2009 Faustino Frechilla
+// Copyright (c) 2009-2010 Faustino Frechilla
+// All rights reserved.
 //
-// This file is part of Blockem.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
 //
-// Blockem is free software: you can redistribute it and/or modify it under the
-// terms of the GNU General Public License as published by the Free Software
-// Foundation, either version 3 of the License, or (at your option) any later
-// version.
+//  1. Redistributions of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//  3. The name of the author may not be used to endorse or promote products
+//     derived from this software without specific prior written permission.
 //
-// Blockem is distributed in the hope that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-// details.
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 //
-// You should have received a copy of the GNU General Public License along
-// with Blockem. If not, see http://www.gnu.org/licenses/.
-//
-/// @file g_blocking_queue.h
-/// @brief This file contains the Blocking queue class.
+/// @file q_blocking_queue.h
+/// @brief Definition of a thread-safe queue based on glib system calls
+/// It internally contains a std::queue which is protected from concurrent
+/// access by glib mutextes and conditional variables
 ///
+/// @author Faustino Frechilla
 /// @history
-/// Ref        Who                When        What
-///            Faustino Frechilla 04-May-2009 Original development
-///            Faustino Frechilla 19-May-2010 Adapted to blockem using glib mutexes
+/// Ref  Who                 When         What
+///      Faustino Frechilla 04-May-2009 Original development (based on pthreads)
+///      Faustino Frechilla 19-May-2010 Ported to glib. Removed pthread dependency
 /// @endhistory
 ///
 // ============================================================================
@@ -32,15 +44,13 @@
 
 #include <glib.h>
 #include <queue>
-
 #include <limits> // std::numeric_limits<>::max
 
-#define BLOCKING_QUEUE_NO_LIMIT         std::numeric_limits<std::size_t >::max()
-#define BLOCKING_QUEUE_DEFAULT_MAX_SIZE BLOCKING_QUEUE_NO_LIMIT
+#define BLOCKING_QUEUE_DEFAULT_MAX_SIZE std::numeric_limits<std::size_t >::max()
 
 /// @brief blocking thread-safe queue
-/// It uses a mutex+condition variables to protect the internal queue implementation.
-/// Inserting or reading elements use the same mutex
+/// It uses a mutex+condition variables to protect the internal queue
+/// implementation. Inserting or reading elements use the same mutex
 template <typename T>
 class BlockingQueue
 {
@@ -49,42 +59,51 @@ public:
     ~BlockingQueue();
 
     /// @brief Check if the queue is empty
-    /// This call can block if another thread owns the lock that protects the queue
+    /// This call can block if another thread owns the lock that protects the
+    /// queue
     /// @return true if the queue is empty. False otherwise
     bool IsEmpty();
 
     /// @brief inserts an element into queue queue
-    /// This call can block if another thread owns the lock that protects the queue. If the queue is full
-    /// The thread will be blocked in this queue until someone else gets an element from the queue
+    /// This call can block if another thread owns the lock that protects the
+    /// queue. If the queue is full The thread will be blocked in this queue
+    /// until someone else gets an element from the queue
     /// @param element to insert into the queue
-    /// @return true if the elem was successfully inserted into the queue. False otherwise
+    /// @return True if the elem was successfully inserted into the queue.
+    ///         False otherwise
     bool Push(const T &a_elem);
 
     /// @brief inserts an element into queue queue
-    /// This call can block if another thread owns the lock that protects the queue. If the queue is full
-    /// The call will return false and the element won't be inserted
+    /// This call can block if another thread owns the lock that protects the
+    /// queue. If the queue is full The call will return false and the element
+    /// won't be inserted
     /// @param element to insert into the queue
-    /// @return true if the elem was successfully inserted into the queue. False otherwise
+    /// @return True if the elem was successfully inserted into the queue.
+    ///         False otherwise
     bool TryPush(const T &a_elem);
 
-    /// @brief extracts an element from the queue (and deletes it from the queue)
-    /// If the queue is empty this call will block the thread until there is something in the queue to be extracted
+    /// @brief extracts an element from the queue (and deletes it from the q)
+    /// If the queue is empty this call will block the thread until there is
+    /// something in the queue to be extracted
     /// @param a reference where the element from the queue will be saved to
     void Pop(T &out_data);
 
-    /// @brief extracts an element from the queue (and deletes it from the queue)
-    /// This call gets the block that protects the queue. It will extract the element from the queue
-    /// only if there are elements in it
+    /// @brief extracts an element from the queue (and deletes it from the q)
+    /// This call gets the block that protects the queue. It will extract the
+    /// element from the queue only if there are elements in it
     /// @param reference to the variable where the result will be saved
-    /// @return bool if the element was retrieved from the queue. False if the qeue was empty
+    /// @return True if the element was retrieved from the queue.
+    ///         False if the queue was empty
     bool TryPop(T &out_data);
 
-    /// @brief extracts an element from the queue (and deletes it from the queue)
-    /// If the queue is empty this call will block the thread until there is something in the queue to be extracted
-    /// or until the timer (2nd parameter) expires
+    /// @brief extracts an element from the queue (and deletes it from the q)
+    /// If the queue is empty this call will block the thread until there
+    /// is something in the queue to be extracted or until the timer
+    /// (2nd parameter) expires
     /// @param reference to the variable where the result will be saved
     /// @param microsecondsto wait before returning if the queue was empty
-    /// @return bool if the element was retrieved from the queue. False if the timeout was reached
+    /// @return True if the element was retrieved from the queue.
+    ///         False if the timeout was reached
     bool TimedWaitPop(T &data, int32_t microsecs);
 
 protected:
@@ -97,7 +116,7 @@ protected:
     GCond*  m_cond;
 };
 
-// include the implementation
+// include the implementation file
 #include "impl/g_blocking_queue_impl.h"
 
 #endif /* _GBLOCKINGQUEUE_H_ */
--- src/impl/g_blocking_queue_impl.h
+++ src/impl/g_blocking_queue_impl.h
@@ -1,28 +1,40 @@
 // ============================================================================
-// Copyright 2009 Faustino Frechilla
+// Copyright (c) 2009-2010 Faustino Frechilla
+// All rights reserved.
 //
-// This file is part of Blockem.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
 //
-// Blockem is free software: you can redistribute it and/or modify it under the
-// terms of the GNU General Public License as published by the Free Software
-// Foundation, either version 3 of the License, or (at your option) any later
-// version.
+//  1. Redistributions of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//  3. The name of the author may not be used to endorse or promote products
+//     derived from this software without specific prior written permission.
 //
-// Blockem is distributed in the hope that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-// details.
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 //
-// You should have received a copy of the GNU General Public License along
-// with Blockem. If not, see http://www.gnu.org/licenses/.
-//
-/// @file
-/// @brief This file contains the implementation of the Blocking queue class.
+/// @file q_blocking_queue_impl.h
+/// @brief Implementation of a thread-safe queue based on glib system calls
+/// It internally contains a std::queue which is protected from concurrent
+/// access by glib mutextes and conditional variables
 ///
+/// @author Faustino Frechilla
 /// @history
-/// Ref        Who                When        What
-///            Faustino Frechilla 04-May-2009 Original development
-///            Faustino Frechilla 19-May-2010 Adapted to blockem using glib mutexes
+/// Ref  Who                 When         What
+///      Faustino Frechilla 04-May-2009 Original development (based on pthreads)
+///      Faustino Frechilla 19-May-2010 Ported to glib. Removed pthread dependency
 /// @endhistory
 ///
 // ============================================================================
@@ -31,9 +43,6 @@
 #define _GBLOCKINGQUEUEIMPL_H_
 
 #include <assert.h>
-#include <errno.h>
-#include <time.h>
-#include <stdint.h> // for uint64_t
 
 #define NANOSECONDS_PER_SECOND 1000000000
 
@@ -41,21 +50,17 @@ template <typename T>
 BlockingQueue<T>::BlockingQueue(std::size_t a_maxSize) :
     m_maximumSize(a_maxSize)
 {
-    m_mutex = g_mutex_new();
-#ifdef DEBUG
-    if (m_mutex == NULL)
+    if (!g_thread_supported ())
     {
-        assert(0);
+        // glib thread system hasn't been initialized yet
+        g_thread_init(NULL);
     }
-#endif
 
-    m_cond = g_cond_new();
-#ifdef DEBUG
-    if (m_cond == NULL)
-    {
-        assert(0);
-    }
-#endif
+    m_mutex = g_mutex_new();
+    m_cond  = g_cond_new();
+
+    assert(m_mutex != NULL);
+    assert(m_cond != NULL);
 }
 
 template <typename T>
