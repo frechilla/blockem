c58277392395940858647fed8efcad89e8daf76f
frechilla <frechilla@b4ae199f-ef0c-4245-bc35-8706ab30845c>
2010-10-01 11:24:42 +0000

    a 1st version of taking the statusbar out of the main window class

---
+++ src/gui_game_statusbar.cpp
@@ -0,0 +1,201 @@
+// ============================================================================
+// Copyright 2010 Faustino Frechilla
+//
+// This file is part of Blockem.
+//
+// Blockem is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// Blockem is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along
+// with Blockem. If not, see http://www.gnu.org/licenses/.
+//
+/// @file gui_game_statusbar.cpp
+/// @brief
+///
+/// @author Faustino Frechilla
+/// @history
+/// Ref       Who                When         What
+///           Faustino Frechilla 01-Oct-2010  Original development
+/// @endhistory
+///
+// ============================================================================
+
+#include "gui_game_statusbar.h"
+
+/// maximum size of the string to apply to score labels
+static const uint32_t SCORE_LABEL_BUFFER_LENGTH = 64;
+
+/// how often stopwatches are updated
+static const uint32_t STOPWATCH_UPDATE_PERIOD_MILLIS = 500; // 1000 = 1 second
+
+
+GameStatusBar::GameStatusBar(BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder>& a_gtkBuilder) throw (GUIException):
+    Gtk::Window(cobject), //Calls the base class constructor
+    m_stopWatchLabelPlayer1(
+        STOPWATCH_UPDATE_PERIOD_MILLIS),
+    m_stopWatchLabelPlayer2(
+        STOPWATCH_UPDATE_PERIOD_MILLIS)
+{
+    // build up the status bar
+    this->pack_start(m_player1ScoreLabel, true, true);
+    this->pack_start(m_arrayStatusBarSeparator[0], false, true);
+    this->pack_start(m_stopWatchLabelPlayer1, true, true);
+    this->pack_start(m_arrayStatusBarSeparator[1], false, true);
+    this->pack_start(m_player2ScoreLabel, true, true);
+    this->pack_start(m_arrayStatusBarSeparator[2], false, true);
+    this->pack_start(m_stopWatchLabelPlayer2, true, true);
+    m_progressBar.set_orientation(Gtk::PROGRESS_LEFT_TO_RIGHT);
+    m_progressBar.set_fraction(0.0);
+    this->pack_start(m_progressBar, true, true);
+    this->show_all();
+}
+
+GameStatusBar::~GameStatusBar()
+{
+}
+
+void GameStatusBar::SetScoreStatus(
+    const Player &a_player1)
+{
+    // this buffer will contain the string to be applied to the labels
+    char theMessage[SCORE_LABEL_BUFFER_LENGTH];
+
+    // calculate the number of squares left
+    int32_t squaresLeftPlayer1 = 0;
+    for (int8_t i = e_minimumPieceIndex ; i < e_numberOfPieces; i++)
+    {
+        if (a_player1.IsPieceAvailable(static_cast<ePieceType_t>(i)))
+        {
+            squaresLeftPlayer1 += a_player1.m_pieces[i].GetNSquares();
+        }
+    }
+    
+    uint8_t red, green, blue;
+    a_player1.GetColour(red, green, blue);
+    
+    snprintf (theMessage,
+              SCORE_LABEL_BUFFER_LENGTH,
+              // i18n TRANSLATORS: the first 3 %02X  will be replaced by the
+              // i18n colour of the current player. The '%s' will be replaced
+              // i18n by this player's name and the '%2d' his/her score
+              // i18n For a better GUI experience all these strings should take
+              // i18n the same amount of characters
+              ngettext("<span color=\"#%02X%02X%02X\">%s</span>: %2d left",
+                       "<span color=\"#%02X%02X%02X\">%s</span>: %2d left",
+                       static_cast<int32_t>(squaresLeftPlayer1)),
+              red,
+              green,
+              blue,
+              player1.GetName().c_str(),
+              squaresLeftPlayer1);
+    m_player1ScoreLabel.set_markup(theMessage);
+}
+
+void GameStatusBar::SetScoreStatus(
+    const Player &a_player1, 
+    const Player &a_player2)
+{
+    // this buffer will contain the string to be applied to the labels
+    char theMessage[SCORE_LABEL_BUFFER_LENGTH];
+    
+    // set score status of player1
+    SetScoreStatus(a_player1);
+
+    // calculate the number of squares left of player2
+    int32_t squaresLeftPlayer1 = 0;
+    int32_t squaresLeftPlayer2 = 0;
+    for (int8_t i = e_minimumPieceIndex ; i < e_numberOfPieces; i++)
+    {
+        if (a_player2.IsPieceAvailable(static_cast<ePieceType_t>(i)))
+        {
+            squaresLeftPlayer2 += a_player2.m_pieces[i].GetNSquares();
+        }
+    }
+
+    uint8_t red, green, blue;
+    a_player2.GetColour(red, green, blue);
+
+    // update player2's GUI widget
+    snprintf (theMessage,
+              SCORE_LABEL_BUFFER_LENGTH,
+              // i18n TRANSLATORS: the first 3 %02X  will be replaced by the
+              // i18n colour of the current player. The '%s' will be replaced
+              // i18n by this player's name and the '%2d' his/her score
+              // i18n For a better GUI experience both strings should take
+              // i18n the same amount of characters
+              ngettext("<span color=\"#%02X%02X%02X\">%s</span>: %2d left",
+                       "<span color=\"#%02X%02X%02X\">%s</span>: %2d left",
+                       static_cast<int32_t>(squaresLeftPlayer2)),
+              red,
+              green,
+              blue,
+              player2.GetName().c_str(),
+              squaresLeftPlayer2);
+    m_player2ScoreLabel.set_markup(theMessage);
+}
+
+void GameStatusBar::SetStopwatchPrefix(
+    const Player &a_player1, 
+    const Player &a_player2)
+{
+    // retrieve the default colour from the  players and use HTML tags so 
+    // the stop watch labels show each player's name written with its 
+    // corresponding colour
+    uint8_t red, green, blue;
+    a_player1.GetColour(red, green, blue);
+    
+    std::stringstream theMessage;
+    theMessage << "<span color=\"#"
+               << std::setfill('0') << std::setw(2) << std::hex << std::uppercase << static_cast<int32_t>(red)
+               << std::setfill('0') << std::setw(2) << std::hex << std::uppercase << static_cast<int32_t>(green)
+               << std::setfill('0') << std::setw(2) << std::hex << std::uppercase << static_cast<int32_t>(blue)
+               << "\">"
+               << a_player1.GetName()
+               << " </span>";
+    m_stopWatchLabelPlayer1.SetPrefix(theMessage.str());
+
+    a_player2.GetColour(red, green, blue);
+
+    theMessage << "<span color=\"#"
+               << std::setfill('0') << std::setw(2) << std::hex << std::uppercase << static_cast<int32_t>(red)
+               << std::setfill('0') << std::setw(2) << std::hex << std::uppercase << static_cast<int32_t>(green)
+               << std::setfill('0') << std::setw(2) << std::hex << std::uppercase << static_cast<int32_t>(blue)
+               << "\">"
+               << a_player2.GetName()
+               << " </span>";
+    m_stopWatchLabelPlayer2.SetPrefix(theMessage.str());
+}
+
+void GameStatusBar::SetFraction(float a_fraction)
+{
+    m_progressBar.set_fraction(a_fraction);
+}
+
+void GameStatusBar::EnablePlayer2(bool a_action)
+{
+    if (a_action == true)
+    {
+        m_arrayStatusBarSeparator[1].show();
+        m_player2ScoreLabel.show();
+        m_arrayStatusBarSeparator[2].show();
+        m_stopWatchLabelPlayer2.show();
+        
+        m_stopWatchLabelPlayer2.Continue();
+    }
+    else
+    {
+        m_arrayStatusBarSeparator[1].hide();
+        m_player2ScoreLabel.hide();
+        m_arrayStatusBarSeparator[2].hide();
+        m_stopWatchLabelPlayer2.hide();
+        
+        m_stopWatchLabelPlayer2.Stop();
+    }
+}
---
+++ src/gui_game_statusbar.h
@@ -0,0 +1,88 @@
+// ============================================================================
+// Copyright 2010 Faustino Frechilla
+//
+// This file is part of Blockem.
+//
+// Blockem is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// Blockem is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along
+// with Blockem. If not, see http://www.gnu.org/licenses/.
+//
+/// @file gui_game_statusbar.h
+/// @brief
+///
+/// @author Faustino Frechilla
+/// @history
+/// Ref       Who                When         What
+///           Faustino Frechilla 01-Oct-2010  Original development
+/// @endhistory
+///
+// ============================================================================
+
+#ifndef GAME_STATUSBAR_H_
+#define GAME_STATUSBAR_H_
+
+#include <string>
+#include <gtkmm.h>
+
+#include "gui_exception.h"
+#include "player.h"
+
+/// @brief class to handle the status bar to be shown on the main window
+class GameStatusBar :
+    public Gtk::HBox
+{
+public:
+    GameStatusBar(BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder>& a_gtkBuilder) throw (GUIException);
+    ~GameStatusBar();
+    
+    /// update score of only player1
+    void SetScoreStatus(const Player &a_player1);
+    
+    /// update score of both player1 and player2
+    void SetScoreStatus(const Player &a_player1, const Player &a_player2);
+    
+    /// set prefix to be shown before the stopwatches values of both
+    /// player1 and player2
+    void SetStopwatchPrefix(const Player &a_player1, const Player &a_player2);
+    
+    /// @param player2 information will be shown if it's true. This data will
+    ///        be hidden if it set to false
+    /// Player2 is enabled by default
+    void EnablePlayer2(bool a_action);
+    
+    /// sets the fraction of the progress bar shown in the status bar
+    /// @param a floating point number between 0.0 to 1.0 (0% to 100%)
+    void SetFraction(float a_fraction);
+    
+private:
+    /// @brief vertical separators to be used in the status bar
+    //TODO that number 3 is magic!!
+    Gtk::VSeparator m_arrayStatusBarSeparator[3];
+
+    /// @brief progress bar for when the computer is "thinking".
+    ///        To be shown in the status bar
+    Gtk::ProgressBar m_progressBar;
+
+    /// @brief label to show the user score in the status bar
+    Gtk::Label m_player1ScoreLabel;
+
+    /// @brief label to show the computer score in the status bar
+    Gtk::Label m_player2ScoreLabel;
+
+    /// @brief show the time the player1 takes to think
+    StopWatchLabel m_stopWatchLabelPlayer1;
+
+    /// @brief show the time the player2 takes to think
+    StopWatchLabel m_stopWatchLabelPlayer2;
+}
+
+#endif // GAME_STATUSBAR_H_
